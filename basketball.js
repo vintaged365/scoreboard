20152478012e81b5704a71c6666a7ebb6e383583
vintaged365 <danolinga00@gmail.com>
2025-11-17 10:50:27 +0300

    Commit initial file

---
+++ basketball.js
@@ -0,0 +1,35 @@
+/*let countEl = document.getElementById("home-score");
+let countEl1 = document.getElementById("guest-score");
+
+let count = 0;
+let count1 = 0;
+
+function increment1() {
+    count += 1;
+    countEl.innerText = count;
+}
+
+function increment2() {
+    count += 2;
+    countEl.innerText = count;
+}
+
+function increment3() {
+    count += 3;
+    countEl.innerText = count;
+}
+
+function increment4() {
+    count1 += 1;
+    countEl1.innerText = count1;
+}
+
+function increment5() {
+    count1 += 2;
+    countEl1.innerText = count1;
+}
+
+function increment6() {
+    count1 += 3;
+    countEl1.innerText = count1;
+}
